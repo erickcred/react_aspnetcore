4e39a7a4e4651d17d18fba37625fb6ca6e007ae8
Erick Rick <erickcredjes@gmail.com>
2023-04-22 18:57:44 -0300

    Refatorando Menu

--- frontend/pro_atividade_app/src/components/Menu.jsx
+++ frontend/pro_atividade_app/src/components/Menu.jsx
@@ -1,5 +1,5 @@
 import * as B from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 const Menu = () => {
   return (
@@ -16,19 +16,35 @@ const Menu = () => {
         <B.Navbar.Toggle aria-controls="basic-nav-bar-nav" />
         <B.Navbar.Collapse id="basic-navbar-nav">
           <B.Nav className="me-auto">
-            <B.Nav.Link as={NavLink} className="nav-link" to="/cliente">
+            <B.Nav.Link
+              as={NavLink}
+              className={(navData) => (navData.isActive ? 'active' : '')}
+              to="/cliente"
+            >
               Clientes
             </B.Nav.Link>
-            <B.Nav.Link as={NavLink} className="nav-link" to="/atividade">
+            <B.Nav.Link
+              as={NavLink}
+              className={(navData) => (navData.isActive ? 'active' : '')}
+              to="/atividade"
+            >
               Atividades
             </B.Nav.Link>
           </B.Nav>
           <B.Nav>
             <B.NavDropdown align="end" title="Erick" id="basic-nav-dropdown">
-              <B.NavDropdown.Item href="#">Perfil</B.NavDropdown.Item>
-              <B.NavDropdown.Item href="#">Configurações</B.NavDropdown.Item>
+              <B.NavDropdown.Item as={Link} to="#">
+                Perfil
+              </B.NavDropdown.Item>
+              <B.NavDropdown.Item as={Link} to="#">
+                Configurações
+              </B.NavDropdown.Item>
+
               <B.NavDropdown.Divider />
-              <B.NavDropdown.Item href="#">Sair</B.NavDropdown.Item>
+
+              <B.NavDropdown.Item as={Link} to="#">
+                Sair
+              </B.NavDropdown.Item>
             </B.NavDropdown>
           </B.Nav>
         </B.Navbar.Collapse>
