32aeccb179fc1d54d63bc6d71125db49d07c5cea
Erick Rick <erickcredjes@gmail.com>
2023-04-22 18:19:50 -0300

    Refatorando e migrando versão dotnet  para 6.0

--- backend/src/ProAtividade.API/Program.cs
+++ backend/src/ProAtividade.API/Program.cs
@@ -1,26 +1,62 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using ProAtividade.Data.Context;
+using ProAtividade.Data.Repositories;
+using ProAtividade.Domain.Interfaces;
+using ProAtividade.Domain.Interfaces.Repositories;
+using ProAtividade.Domain.Interfaces.Services;
+using ProAtividade.Domain.Services;
 
-namespace ProAtividade.API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<Contexto>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
+
+builder.Services.AddScoped<IAtividadeRepository, AtividadeRepository>();
+builder.Services.AddScoped<IGeralRepository, GeralRepository>();
+builder.Services.AddScoped<IAtividadeService, AtividadeService>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(
+        options => // Configuração para trazer as Strings dos enums ao invez dos números
         {
-            CreateHostBuilder(args).Build().Run();
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
         }
+);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProAtividade.API", Version = "v1" });
+});
+builder.Services.AddCors();
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProAtividade.API v1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors(option => option.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin());
+app.MapControllers();
+
+app.UseRouting();
+
+app.Run();
+
+
+
